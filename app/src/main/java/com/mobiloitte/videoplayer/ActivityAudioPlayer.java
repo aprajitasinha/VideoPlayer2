f5a3ac950a22726826426cf2031ee34fdee80e7d
aprajita <an-aprajitasinha@mobiloitte.com>
2019-04-05 16:56:46 +0530

    First commit

---
+++ app/src/main/java/com/mobiloitte/videoplayer/ActivityAudioPlayer.java
@@ -0,0 +1,143 @@
+package com.mobiloitte.videoplayer;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class ActivityAudioPlayer extends AppCompatActivity {
+
+    private TextView textMaxTime;
+    private TextView textCurrentPosition;
+    private Button buttonPause;
+    private Button buttonStart;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+    private Handler threadHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+
+
+
+
+        this.textCurrentPosition = (TextView)this.findViewById(R.id.textView_currentPosion);
+        this.textMaxTime=(TextView) this.findViewById(R.id.textView_maxTime);
+        buttonStart= (Button) findViewById(R.id.button_start);
+        this.buttonPause= (Button) this.findViewById(R.id.button_pause);
+
+        this.buttonPause.setEnabled(false);
+
+        this.seekBar= (SeekBar) this.findViewById(R.id.seekBar);
+        this.seekBar.setClickable(false);
+
+        // ID of 'mysong' in 'raw' folder.
+        int songId = this.getRawResIdByName("video");
+
+        // Create MediaPlayer.
+        this.mediaPlayer=   MediaPlayer.create(this, songId);
+    }
+
+    // Find ID of resource in 'raw' folder.
+    public int getRawResIdByName(String resName)  {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        return resID;
+    }
+
+    // Convert millisecond to string.
+    private String millisecondsToString(int milliseconds)  {
+        long minutes = TimeUnit.MILLISECONDS.toMinutes((long) milliseconds);
+        long seconds =  TimeUnit.MILLISECONDS.toSeconds((long) milliseconds) ;
+        return minutes+":"+ seconds;
+    }
+
+
+    public void doStart(View view)  {
+        // The duration in milliseconds
+        int duration = this.mediaPlayer.getDuration();
+
+        int currentPosition = this.mediaPlayer.getCurrentPosition();
+        if(currentPosition== 0)  {
+            this.seekBar.setMax(duration);
+            String maxTimeString = this.millisecondsToString(duration);
+            this.textMaxTime.setText(maxTimeString);
+        } else if(currentPosition== duration)  {
+            // Resets the MediaPlayer to its uninitialized state.
+            this.mediaPlayer.reset();
+        }
+        this.mediaPlayer.start();
+        // Create a thread to update position of SeekBar.
+        UpdateSeekBarThread updateSeekBarThread= new UpdateSeekBarThread();
+        threadHandler.postDelayed(updateSeekBarThread,50);
+
+        this.buttonPause.setEnabled(true);
+        this.buttonStart.setEnabled(false);
+    }
+
+    // Thread to Update position for SeekBar.
+    class UpdateSeekBarThread implements Runnable {
+
+        public void run()  {
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            String currentPositionStr = millisecondsToString(currentPosition);
+            textCurrentPosition.setText(currentPositionStr);
+
+            seekBar.setProgress(currentPosition);
+            // Delay thread 50 milisecond.
+            threadHandler.postDelayed(this, 50);
+        }
+    }
+
+    // When user click to "Pause".
+    public void doPause(View view)  {
+        this.mediaPlayer.pause();
+        this.buttonPause.setEnabled(false);
+        this.buttonStart.setEnabled(true);
+    }
+
+    // When user click to "Rewind".
+    public void doRewind(View view)  {
+        int currentPosition = this.mediaPlayer.getCurrentPosition();
+        int duration = this.mediaPlayer.getDuration();
+        // 5 seconds.
+        int SUBTRACT_TIME = 5000;
+
+        if(currentPosition - SUBTRACT_TIME > 0 )  {
+            this.mediaPlayer.seekTo(currentPosition - SUBTRACT_TIME);
+        }
+    }
+
+    // When user click to "Fast-Forward".
+    public void doFastForward(View view)  {
+        int currentPosition = this.mediaPlayer.getCurrentPosition();
+        int duration = this.mediaPlayer.getDuration();
+        // 5 seconds.
+        int ADD_TIME = 5000;
+
+        if(currentPosition + ADD_TIME < duration)  {
+            this.mediaPlayer.seekTo(currentPosition + ADD_TIME);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/mobiloitte/videoplayer/MainActivity.java
@@ -0,0 +1,138 @@
+package com.mobiloitte.videoplayer;
+
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+    MediaPlayer mediaPlayer;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    boolean pausing = false;
+    private int position = 0;
+    MediaController mediaC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button buttonPlayVideo = ( Button ) findViewById( R.id.playvideoplayer );
+        getWindow().setFormat( PixelFormat.UNKNOWN );
+        final VideoView mVideoView = ( VideoView ) findViewById( R.id.videoview );
+
+
+
+        if (mediaC == null) {
+            mediaC = new MediaController(MainActivity.this);
+            mediaC.setAnchorView(mVideoView);
+            mVideoView.setMediaController(mediaC);
+        }
+        try {
+
+            int id = this.getRawResIdByName("video");
+            mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + id));
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mVideoView.seekTo( position );
+                if (position == 0) {
+                    mVideoView.start();
+                }
+                mediaPlayer.setOnVideoSizeChangedListener( new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        mediaC.setAnchorView( mVideoView );
+                    }
+
+                } );
+            }
+        });
+
+
+
+
+       /* Bundle bundle = getIntent().getExtras();*/
+      /*  if (bundle != null) {
+            mVideoView = bundle.getString(Const "android.resource://com.example.demopageview/" + R.raw.video;);
+        }*/
+
+
+      String uriPath = "android.resource://com.mobiloitte.videoplayer/" + R.raw.videoplayer;
+
+
+       // String uriPath="https://youtu.be/o-1dMLJwOe0";
+        Uri uri = Uri.parse( uriPath );
+        mVideoView.setVideoURI( uri );
+        mVideoView.requestFocus();
+        mVideoView.start();
+
+        buttonPlayVideo.setOnClickListener( new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                VideoView mVideoView = (VideoView) findViewById( R.id.videoview );
+
+               String uriPath = "android.resource://com.mobiloitte.videoplayer/" + R.raw.videoplayer;
+           //String uriPath="https://youtu.be/o-1dMLJwOe0";
+                Uri uri = Uri.parse( uriPath );
+                mVideoView.setVideoURI( uri );
+                mVideoView.requestFocus();
+                mVideoView.start();
+
+                mVideoView.setVideoPath(  "android.resource://com.mobiloitte.videoplayer/" + R.raw.video );
+              //  mVideoView.setVideoPath(  "https://youtu.be/o-1dMLJwOe0" );
+                mVideoView.setMediaController( mediaC );
+                mVideoView.start();
+            }
+
+
+        } );
+
+
+    }
+
+    private int getRawResIdByName(String video) {
+        String pkgName = this.getPackageName();
+        int resID = this.getResources().getIdentifier(video, "raw", pkgName);
+        Log.i("AndroidVideoView", "Res Name: " + video + "==> Res ID = " + resID);
+
+        return resID;
+    }
+
+
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
